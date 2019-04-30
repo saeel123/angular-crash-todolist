a7a2457685bd19b2919c1bb0ce276dd24e5ec107
Saeel Parulekar <saeelmacman123@gmail.com>
2019-04-30 08:15:39 +0530

    first commit

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from "../../models/Todo";
+import { TodoService } from "../../services/todo.service";
+
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  //Set dynamic classes
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo){
+    //toggle ui
+    todo.completed = !todo.completed;
+
+    //toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => 
+      console.log(todo));
+  }
+
+  onDelete(todo){
+    todo.completed = !todo.completed;
+  }
+
+  
+
+}
---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from "../../services/todo.service";
+
+import { Todo } from "../../models/Todo";
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  todos:Todo[];
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+}
